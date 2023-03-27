af14434b760a753839725197537f40f7768f7c22
Briggs Baltzell <BriggsBaltzellB3@gmail.com>
2023-03-27 08:22:27 -0500

    Implemented setContentString

--- modules/src/resource/seadXmlElement.cpp
+++ modules/src/resource/seadXmlElement.cpp
@@ -1,3 +1,5 @@
+#include <algorithm>
+#include <cstring>
 #include <resource/seadXmlElement.h>
 
 namespace sead
@@ -20,6 +22,17 @@ void XmlElement::setContent(u8* content, u32 content_length, bool owns_content)
         mContent = content;
         mContentLength = content_length;
     }
+}
+
+void XmlElement::setContentString(const SafeString& content, Heap* heap)
+{
+    auto len = content.calcLength();
+    auto* con = new (heap) u8[len + 1];
+
+    memcpy(con, content.cstr(), len);
+    con[len] = 0;
+
+    setContent(con, len, true);
 }
 
 }  // namespace sead
